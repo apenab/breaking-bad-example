2f26418e9baec2a696cc3144974e7a7fe1a73c37
Antonio Peña Batista <apenab1995@gmail.com>
2022-03-28 18:20:58 +0200

    feat: added switch language feature

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,14 +1,77 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import * as React from "react";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import TranslateIcon from "@mui/icons-material/Translate";
 import { useTranslation } from "react-i18next";
 
 export function NavBar() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  function handleMenu(event: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  function handleChangeLanguage(language: "en" | "es") {
+    i18n.changeLanguage(language);
+    handleClose();
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {t("nav.title").toUpperCase()}
         </Typography>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleMenu}
+          color="inherit"
+        >
+          <TranslateIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            selected={i18n.language === "en"}
+            onClick={() => handleChangeLanguage("en")}
+          >
+            English
+          </MenuItem>
+          <MenuItem
+            selected={i18n.language === "es"}
+            onClick={() => handleChangeLanguage("es")}
+          >
+            Español
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 import translationEN from "./en/translation.json";
@@ -13,18 +14,26 @@ export const resources = {
   },
 } as const;
 
+const DETECTION_OPTIONS = {
+  order: ["localStorage", "navigator"],
+  caches: ["localStorage"],
+};
+
 i18n
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources,
+    detection: DETECTION_OPTIONS,
     fallbackLng: "en",
-    debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    resources,
   });
 
 export default i18n;
